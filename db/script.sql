316f2aeeb45a1acb91e06d2c79f2ce706871a930
camilabersan <camilabersan.564@gmail.com>
2024-04-22 10:57:48 -0300

    feat: change table

--- db/script.sql
+++ db/script.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY, 
-    nome VARCHAR(100) NOT NULL, 
+    nome VARCHAR(100) NOT NULL,
+    sobrenome VARCHAR(100) NOT NULL, 
     email VARCHAR(100) NOT NULL,
     idade INTEGER NOT NULL,
     signo VARCHAR(20) NOT NULL,
     datanascimento DATE NOT NULL,
-    status VARCHAR(20) NOT NULL,
     sexo VARCHAR(10) NOT NULL
 );
